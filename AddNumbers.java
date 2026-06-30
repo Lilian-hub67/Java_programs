3b1cb639d37a1e6ef136ca34d2c70fec16ad31ac
Lilian-hub67 <liliankirigo67.com>
2026-06-30 17:25:56 +0300

    AddNumbers

---
+++ AddNumbers.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class AddNumbers {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = input.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = input.nextInt();
+
+        System.out.println("Sum = " + (a + b));
+    }
+}
